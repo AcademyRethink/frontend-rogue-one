163e088f6e43435c2b55344c003ddf226805b22c
luan <luan.barreiros@rethink.dev>
2023-06-29 14:57:43 -0300

    feat: created static sidebar

---
+++ src/view/playground/index.tsx
@@ -0,0 +1,14 @@
+import Sidebar from '../../layout/sidebar/index';
+import './styles.css';
+
+const Playground = (...args: any) => {
+  return (
+    <>
+      <div>
+        <Sidebar />
+      </div>
+    </>
+  );
+};
+
+export default Playground;
