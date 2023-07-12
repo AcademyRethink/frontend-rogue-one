cce8638512ecab7ece91130c77362b5cfc25f085
Vinícius Delmo <vinicius.delmo@rethink.dev>
2023-07-12 17:12:18 -0300

    refactor: removing truncate function from card

--- src/layout/TopBar/NotificationStatus/NotificationCard/NotificationCard.tsx
+++ src/layout/TopBar/NotificationStatus/NotificationCard/NotificationCard.tsx
@@ -1,36 +1,11 @@
-import { useEffect, useRef } from 'react';
 import styles from './style.module.scss';
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
 import { Notification } from '../../../../types/notificationsTypes';
 
 const NotificationCard = ({ message }: Notification) => {
-  const MAX_WORDS = 9; // Define o máximo de palavras a serem exibidas
-  const paragraphRef = useRef<HTMLParagraphElement>(null);
-
-  useEffect(() => {
-    if (paragraphRef.current) {
-      tippy(paragraphRef.current, {
-        content: message,
-        placement: 'top',
-        arrow: true,
-       
-      });
-    }
-  }, [message]);
-
-  // Função para truncar a mensagem
-  const truncateMessage = (message: string) => {
-    const words = message.split(' ');
-    if (words.length > MAX_WORDS) {
-      return words.slice(0, MAX_WORDS).join(' ') + '...';
-    }
-    return message;
-  };
-
+  
   return (
     <div className={styles.cardNotification}>
-      <p ref={paragraphRef}>{truncateMessage(message)}</p>
+      <p>{message}</p>
     </div>
   );
 };
