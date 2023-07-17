844d6b783fde5a39d5107ac38b8b2b176d4efadf
Vinícius Delmo <vinicius.delmo@rethink.dev>
2023-07-17 13:12:03 -0300

    build: stylling

--- src/view/myProfile/LineTable/LineTable.tsx
+++ src/view/myProfile/LineTable/LineTable.tsx
@@ -6,8 +6,13 @@ const LineTable = (props: any) => {
   return (
     <div className={style.lineContainer}>
       <div className={style.lineInformation}>
+        <div className={style.titleContainer}>
         <h2 className={style.title}>{title}</h2>
+        </div>
+        <div className={style.valueContainer}>
+
         <h4 className={style.value}>{value}</h4>
+        </div>
       </div>
       <img src={dividerMyProfile} alt="" />
     </div>
