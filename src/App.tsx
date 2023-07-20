104a22bcac12064a58aab28238e7a0c1966b346c
Matheus Dias <matheus.dias@rethink.dev>
2023-07-20 13:50:17 -0300

    fix: App.tsx back to normal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import BestSellerChart from './components/BestSellerChart';
 //TODO: mudar container de filtros de dentro pra fora dos containers de grafico
 function App() {
   return (
     <>
-      <BestSellerChart />
     </>
   );
 }
