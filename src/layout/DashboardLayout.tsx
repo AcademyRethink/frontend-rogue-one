8d5bd5242dd0b5b43795bc9020578d6a92b7f16e
Matheus Dias <matheus.dias@rethink.dev>
2023-07-24 18:20:37 -0300

    feat: dinamic date on top bar

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -3,22 +3,33 @@ import TopBar from './TopBar/TopBar';
 import Sidebar from './sidebar/Sidebar';
 import style from './style.module.scss';
 import { useEffect, useState } from 'react';
+import { getLastDate } from '../services/report';
+import dayjs from 'dayjs';
 
 const DashboardLayout = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [lastUpdateDate, setLastUpdateDate] = useState<string>('');
 
   const sessionData = localStorage.getItem('session');
   const token = sessionData ? JSON.parse(sessionData).token : '';
+  const cnpj = sessionData ? JSON.parse(sessionData).cnpj : '';
 
   useEffect(() => {
     if (token) setIsLoggedIn(true);
   }, [token]);
+
+  useEffect(() => {
+    if (cnpj)
+      getLastDate(cnpj).then((res) =>
+        setLastUpdateDate(dayjs(res).format('DD/MM/YYYY'))
+      );
+  }, [cnpj]);
   return (
     <div className={style.dashboardLayoutContainer}>
       {location.pathname !== '/' && isLoggedIn ? (
         <>
           <header className={style.dashboardLayoutTopBar}>
-            <TopBar updateDate="20/07/2023" />
+            <TopBar updateDate={lastUpdateDate} />
           </header>
           <aside className={style.dashboardLayoutSideBar}>
             <Sidebar />
