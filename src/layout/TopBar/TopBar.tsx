8fc5a49cd8c90dc36b4445bca071553f0dc83187
Matheus Dias <matheus.dias@rethink.dev>
2023-07-24 16:54:08 -0300

    feat: notification close on click outside

--- src/layout/TopBar/TopBar.tsx
+++ src/layout/TopBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './style.module.scss';
 import notification from '../../assets/notifications.svg';
 import NotificationStatus from './NotificationStatus/NotificationStatus';
@@ -8,24 +8,45 @@ import dividerTopBar from '../../assets/dividerTopBar.svg';
 const TopBar = (props: TopBarProps) => {
   const { updateDate } = props;
   const [showModal, setShowModal] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   const toggleModal = () => {
     setShowModal(!showModal);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.topBarContainer}>
       <div className={styles.leftMenu}>
         <h1>Visão geral</h1>
-        <img src={dividerTopBar} alt="Linha horizontal, com função de ser um divisor de conteúdo" />
+        <img
+          src={dividerTopBar}
+          alt="Linha horizontal, com função de ser um divisor de conteúdo"
+        />
         <p>Dados de mercado atualizados em: {updateDate}</p>
       </div>
-      
+
       <button onClick={toggleModal}>
-        <img src={notification} alt="Ícone de sino, função de exibir todas as notificações" />
+        <img
+          src={notification}
+          alt="Ícone de sino, função de exibir todas as notificações"
+        />
       </button>
       {showModal && (
-        <div className={styles.modalWrapper}>
+        <div className={styles.modalWrapper} ref={modalRef}>
           <div className={styles.modalOverlay} onClick={toggleModal} />
           <div className={styles.modalContent}>
             <NotificationStatus />
